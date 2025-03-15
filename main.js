6ad99c7742c47051c9c50ce377dc468cb0d67bf9
Ctatiana29 <tatiana.caceres@est.ulsa.edu.ni>
2025-03-14 20:50:02 -0600

    main

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 const accordionBtns= document.querySelectorAll('.accordion-btn')
 
 accordionBtns.forEach(btn => {
-    this.classList.toggle('active')
-    const accordionDescription= this.querySelector('.plus-icon')
-    const plusIcon= this.querySelector('.plus-icon')
-    const minusIcon= this.querySelector('.minus-icon')
-
-    if(accordionDescription.style.maxHeight){
-        accordionDescription.style.maxHeight=null
-        plusIcon.style.display='block'
-        munusIcon.style.display=none
-    }else{
-        accordionDescription.style.maxHeight=accordionDescription.scrollHeight+'px'
-        plusIcon.style.display='none'
-        minusIcon.style.display='block'
+    btn.addEventListener('click',function(){
+        this.classList.toggle('active')
+        const accordionDescription= this.nextElementSibling
+        const plusIcon= this.querySelector('.plus-icon')
+        const minusIcon= this.querySelector('.minus-icon')
+        
+        if(accordionDescription.style.maxHeight){
+            accordionDescription.style.maxHeight=null
+            plusIcon.style.display='block'
+            minusIcon.style.display=none
+        }else{
+            accordionDescription.style.maxHeight=accordionDescription.scrollHeight+'px'
+            plusIcon.style.display='none'
+            minusIcon.style.display='block'
     }
+
+    })
+    
     
 })
